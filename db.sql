bfac913119c08e0416b6518dbd0466364ce62545
PabloSosaUy <104596993+PabloSosaUy@users.noreply.github.com>
2022-07-08 21:38:45 -0300

    Index creado

--- db.sql
+++ db.sql
@@ -1,7 +1,18 @@
+create database blogPost;
+use blogpost;
+
 CREATE TABLE usuario(
     id int primary key auto_increment,
     username varchar(50) unique,
     complete_name varchar(50),
     password varchar(255)
+);
+
+CREATE TABLE publicaciones (
+	id int primary key auto_increment,
+	autor VARCHAR(40),
+    cuerpo VARCHAR(255),
+	fechaYHora DATETIME,
+    foreign key (autor) references usuario(username)
 );
 
