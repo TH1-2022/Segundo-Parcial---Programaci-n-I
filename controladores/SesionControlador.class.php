1cda1aaa5fe4bebb8d7d73464322237ffcfb17b0
PabloSosaUy <104596993+PabloSosaUy@users.noreply.github.com>
2022-07-08 23:53:52 -0300

    intento de hacer funcionar Sesion Controlador
    no avanza mas de la linea 10

--- controladores/SesionControlador.class.php
+++ controladores/SesionControlador.class.php
@@ -1,10 +1,14 @@
 <?php 
     require "../utils/autoload.php";
+    require "../modelos/UsuarioModelo.class.php";
 
     class SesionControlador {
         public static function IniciarSesion($context){
+            
+            $username = $_POST['username'];
+            $password = $_POST['password'];
             $u = new UsuarioModelo();
-            $u -> Nombre = $context['post']['usuario'];
+            $u -> Nombre = $context['post']['username'];
             $u -> Password = $context['post']['password'];
             if($u -> Autenticar($u -> Nombre, $u -> Password)){
                 SessionCreate("autenticado",true);
